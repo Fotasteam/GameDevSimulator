ad3ff54a1f21fb3843529ab2399c0e9d01ad6cd5
Fota Steam <fotasteam@gmail.com>
2023-04-10 13:24:01 +0200

    Dodaj pliki projektów.

---
+++ RateGameAlgorythm/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Input data to generate a game review.");
+
+string input = Console.ReadLine();
+int ammoutOfCommas = 0;
+
+string genre1, genre2;
+string platform;
+int devTime;
+string budget, publisher;
+string topic1, topic2, topic3;
+string marketing;
+int ageRating;
+
+foreach (char character in input)
+{
+    if (character ==  ',') ammoutOfCommas++;
+}
+
+for (int i = 0; i < ammoutOfCommas; ++i)
+{
+
+}
