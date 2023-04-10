6b057b787dfbd5edfbfc53e93f7a6abd20fba37e
Fota Steam <fotasteam@gmail.com>
2023-04-10 13:38:55 +0200

    dsa

--- RateGameAlgorythm/Program.cs
+++ RateGameAlgorythm/Program.cs
@@ -1,22 +1,33 @@
 ﻿Console.WriteLine("Input data to generate a game review.");
 
 string input = Console.ReadLine();
-int ammoutOfCommas = 0;
+int ammountOfCommas = 0;
 
+int genres, topics;
 string genre1, genre2;
 string platform;
 int devTime;
 string budget, publisher;
 string topic1, topic2, topic3;
 string marketing;
-int ageRating;
+int ageRating, dimention;
+List<int> resourceAllocation = new List<int>();
+bool monetization; int multiplayer;
+
+List<string> sequencesOfInput = new List<string>();
 
 foreach (char character in input)
 {
-    if (character ==  ',') ammoutOfCommas++;
+    if (character == ',') ++ammountOfCommas;
 }
 
-for (int i = 0; i < ammoutOfCommas; ++i)
+for (int j = 0; j < ammountOfCommas; ++j)
 {
-
+    int i = 0;
+    string currentSequence = "";
+    while (input[i] != ',')
+    {
+        currentSequence += i;
+        ++i;
+    }
 }
