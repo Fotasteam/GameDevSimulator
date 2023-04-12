1fb14b059e25605874f23341dfd578de4eb27ed9
Fota Steam <fotasteam@gmail.com>
2023-04-12 23:07:38 +0200

    .

--- RateGameAlgorythm/Program.cs
+++ RateGameAlgorythm/Program.cs
@@ -3,7 +3,6 @@
 string input = Console.ReadLine();
 int ammountOfCommas = 0;
 
-int genres, topics;
 string genre1, genre2;
 string platform;
 int devTime;
@@ -14,27 +13,42 @@ int ageRating, dimention;
 List<int> resourceAllocation = new List<int>();
 bool monetization; int multiplayer;
 
-List<string> sequencesOfInput = new List<string>();
+//List<string> sequencesOfInput = new List<string>();
 
-foreach (char character in input)
-{
-    if (character == ',') ++ammountOfCommas;
-}
+//foreach (char character in input)
+//{
+//    if (character == ',') ++ammountOfCommas;
+//}
 
-int i = 0;
-for (int j = 0; j < ammountOfCommas; ++j)
-{
-    string currentSequence = "";
-    while (input[i] != ',')
-    {
-        currentSequence += input[i];
-        ++i;
-    }
-    ++i;
-    sequencesOfInput.Add(currentSequence);
-}
+//int j = 0;
+//for (int i = 0; i < ammountOfCommas; ++i)
+//{
+//    string currentSequence = "";
+//    while (input[j] != ',')
+//    {
+//        currentSequence += input[j];
+//        ++j;
+//    }
+//    ++j;
+//    sequencesOfInput.Add(currentSequence);
+//}
 
-foreach (string seq in sequencesOfInput)
-{
-    Console.WriteLine(seq);
-}
+//for (int i = 0; i < sequencesOfInput.Count; ++i)
+//{
+//    Console.WriteLine(sequencesOfInput[i]);
+
+//    switch (i)
+//    {
+//        case 0:
+            
+//            break;
+
+//        case 1:
+//            break;
+
+//        default:
+//            Console.WriteLine("Something went wrong...");
+//            break;
+//    }
+//}
+
