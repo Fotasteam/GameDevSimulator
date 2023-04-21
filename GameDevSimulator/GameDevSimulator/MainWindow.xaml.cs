cda1cd93ba18c91838036fda18f7643016e0e5d4
Fota Steam <fotasteam@gmail.com>
2023-04-21 20:40:19 +0200

    various things

--- GameDevSimulator/GameDevSimulator/MainWindow.xaml.cs
+++ GameDevSimulator/GameDevSimulator/MainWindow.xaml.cs
@@ -30,10 +30,5 @@ namespace GameDevSimulator
         {
             this.InitializeComponent();
         }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
     }
 }
